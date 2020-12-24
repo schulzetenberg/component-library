68011397f3ebd03893c24fd6f25adf2ca80c7aee
Jacob Schulzetenberg <jacob.schulzetenberg@gmail.com>
2020-12-23 18:50:48 -0600

    Additional utils

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -4,3 +4,10 @@ export const parseCamelCase = (text = ''): string => {
     return str.toUpperCase();
   });
 };
+
+export async function asyncForEach(array: any, callback: Function): Promise<any> {
+  for (let index = 0; index < array.length; index += 1) {
+    // eslint-disable-next-line no-await-in-loop
+    await callback(array[index], index, array);
+  }
+};
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,11 +1,14 @@
-import { parseCamelCase } from './helpers';
+import { parseCamelCase, asyncForEach } from './helpers';
 import Request from './request';
 import { setSessionCookie, SessionContext, SessionProvider } from './session-context';
+import usePrevious from './use-previous';
 
 export {
 	parseCamelCase,
+	asyncForEach,
 	Request,
 	setSessionCookie,
 	SessionContext,
 	SessionProvider,
+	usePrevious,
 };

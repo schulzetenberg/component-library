d56e2214b090f3b26938ca5a347bc4e486608ac6
Jacob <jacob.schulzetenberg@gmail.com>
2022-04-10 22:15:50 -0500

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,5 @@
 module.exports = {
   root: true,
   extends: ['@schulzetenberg/eslint-config-react'],
-  rules: {
-    'react/function-component-definition': 0, // TODO: remove
-  },
+  rules: {},
 };
